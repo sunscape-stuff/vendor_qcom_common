3e9d23a88b5aa4edccfc128d1b4cf19a207579b6
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-11-07 19:24:47 +0000

    common: Adapt to AOSP (sync with qcom/common device tree)
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- system/perf/perf-vendor.mk
+++ system/perf/perf-vendor.mk
@@ -69,9 +69,7 @@ PRODUCT_COPY_FILES += \
     vendor/qcom/common/system/perf/proprietary/system_ext/lib64/vendor.qti.qspmhal@1.0.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/vendor.qti.qspmhal@1.0.so
 
 PRODUCT_PACKAGES += \
-    PowerSaveMode \
     workloadclassifier \
-    PerformanceMode \
     QPerformance \
     QXPerformance \
     UxPerformance
